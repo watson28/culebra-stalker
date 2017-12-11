7594e197eb141b5372b790f60137f561adf38104
Jorge Florian <jafn28@gmail.com>
2017-12-11 08:12:21 -0500

    fix routing

--- src/client/client.js
+++ src/client/client.js
@@ -2,19 +2,14 @@ import { render } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import { Router, browserHistory, match } from 'react-router';
 import reducer from 'reducers/index';
 import routes from 'components/routes';
 
 const store = createStore(reducer, null, applyMiddleware(thunk));
 
-console.log(match);
-
-match({ history: browserHistory, routes }, (error, redirectLocation, renderProps) => {
-    render(
-        <Provider store={store}>
-            <Router {...renderProps} />
-        </Provider>,
-        document.getElementById('root')
-    );
-})
+render(
+    <Provider store={store}>
+        <h1>Hola mundo!</h1>
+    </Provider>,
+    document.getElementById('root')
+);
